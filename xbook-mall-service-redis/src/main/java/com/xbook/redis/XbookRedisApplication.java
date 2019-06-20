f318eb56efdc706be695a526e73ca308ea177fa0
pingjin <736252868@qq.com>
2019-06-20 18:22:51 +0800

    更改扫描方式

--- xbook-mall-service-redis/src/main/java/com/xbook/redis/XbookRedisApplication.java
+++ xbook-mall-service-redis/src/main/java/com/xbook/redis/XbookRedisApplication.java
@@ -1,10 +1,11 @@
 package com.xbook.redis;
 
-import javafx.application.Application;
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableDubbo(scanBasePackages = "com.xbook.redis.service.impl")
 public class XbookRedisApplication {
 
     public static void main(String[] args) {
