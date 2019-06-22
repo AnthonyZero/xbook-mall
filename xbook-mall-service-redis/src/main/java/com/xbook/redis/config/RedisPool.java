a1bfb06f363e0442ea8d7056e9593742cca9cb77
Anthony <736252868@qq.com>
2019-06-22 23:44:53 +0800

    防止密码未设置的时候 配置是空字符串 redis以为需要AUTH

--- xbook-mall-service-redis/src/main/java/com/xbook/redis/config/RedisPool.java
+++ xbook-mall-service-redis/src/main/java/com/xbook/redis/config/RedisPool.java
@@ -1,5 +1,6 @@
 package com.xbook.redis.config;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
@@ -21,8 +22,12 @@ public class RedisPool {
         jedisPoolConfig.setMaxTotal(redisConfig.getPoolMaxTotal());
         jedisPoolConfig.setMaxIdle(redisConfig.getPoolMaxIdle());
         jedisPoolConfig.setMaxWaitMillis(redisConfig.getPoolMaxWait() * 1000);
+        String password = null;
+        if (StringUtils.isNotBlank(redisConfig.getPassword())) {
+            password = redisConfig.getPassword(); //防止密码未设置的时候 配置是空字符串 redis以为需要AUTH
+        }
         JedisPool jedisPool = new JedisPool(jedisPoolConfig, redisConfig.getHost(), redisConfig.getPort(),
-                redisConfig.getTimeout() * 1000, redisConfig.getPassword(), 0);
+                redisConfig.getTimeout() * 1000, password, 0);
         return jedisPool;
     }
 }
