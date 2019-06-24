7ee4bb795f3f52cd632d0c680599790809605d50
pingjin <736252868@qq.com>
2019-06-24 18:52:06 +0800

    添加redis相关基础操作

--- xbook-mall-api/xbook-mall-redis-api/src/main/java/com/xbook/redis/service/RedisService.java
+++ xbook-mall-api/xbook-mall-redis-api/src/main/java/com/xbook/redis/service/RedisService.java
@@ -6,9 +6,59 @@ public interface RedisService {
 
     /**
      * 获取String 值
+     *
      * @param prefix
      * @param key
      * @return
      */
     String get(KeyPrefix prefix, String key);
+
+    /**
+     * 设置值
+     *
+     * @param prefix
+     * @param key
+     * @param value
+     * @return
+     */
+    boolean set(KeyPrefix prefix, String key, String value);
+
+
+    /**
+     * 判断Key是否存在
+     *
+     * @param prefix
+     * @param key
+     * @return
+     */
+    boolean exists(KeyPrefix prefix, String key);
+
+
+    /**
+     * 自增
+     *
+     * @param prefix
+     * @param key
+     * @return
+     */
+    Long incr(KeyPrefix prefix, String key);
+
+
+    /**
+     * 自减
+     * @param prefix
+     * @param key
+     * @return
+     */
+    Long decr(KeyPrefix prefix, String key);
+
+
+
+    /**
+     * 删除键
+     * @param prefix
+     * @param key
+     * @return
+     */
+    public boolean delete(KeyPrefix prefix, String key);
 }
--- xbook-mall-service-redis/src/main/java/com/xbook/redis/service/impl/RedisClient.java
+++ xbook-mall-service-redis/src/main/java/com/xbook/redis/service/impl/RedisClient.java
@@ -4,6 +4,7 @@ import com.alibaba.dubbo.config.annotation.Service;
 import com.xbook.common.redis.KeyPrefix;
 import com.xbook.redis.service.RedisService;
 import org.springframework.beans.factory.annotation.Autowired;
+import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
 @Service
@@ -14,6 +15,97 @@ public class RedisClient implements RedisService {
 
     @Override
     public String get(KeyPrefix prefix, String key) {
-        return jedisPool.getResource().get(key);
+        Jedis jedis = null;
+        try {
+            jedis = jedisPool.getResource();
+            // 生成真正的key
+            String realKey = prefix.getPrefix() + key;
+            String str = jedis.get(realKey);
+            return str;
+        } finally {
+            returnToPool(jedis);
+        }
+    }
+
+    @Override
+    public boolean set(KeyPrefix prefix, String key, String value) {
+        Jedis jedis = null;
+        try {
+            jedis = jedisPool.getResource();
+            if (value == null || value.length() <= 0) {
+                return false;
+            }
+            // 生成真正的key
+            String realKey = prefix.getPrefix() + key;
+            int seconds = prefix.expireSeconds();
+            if (seconds < 1) { //不过期
+                jedis.set(realKey, value);
+            } else {
+                jedis.setex(realKey, seconds, value);
+            }
+            return true;
+        }finally {
+            returnToPool(jedis);
+        }
+    }
+
+    @Override
+    public boolean exists(KeyPrefix prefix, String key) {
+        Jedis jedis = null;
+        try {
+            jedis = jedisPool.getResource();
+            String realKey = prefix.getPrefix() + key;
+            return jedis.exists(realKey);
+        } finally {
+            returnToPool(jedis);
+        }
+    }
+
+    @Override
+    public Long incr(KeyPrefix prefix, String key) {
+        Jedis jedis = null;
+        try {
+            jedis = jedisPool.getResource();
+            String realKey = prefix.getPrefix() + key;
+            return jedis.incr(realKey);
+        } finally {
+            returnToPool(jedis);
+        }
+    }
+
+    @Override
+    public Long decr(KeyPrefix prefix, String key) {
+        Jedis jedis = null;
+        try {
+            jedis = jedisPool.getResource();
+            String realKey = prefix.getPrefix() + key;
+            return jedis.decr(realKey);
+        } finally {
+            returnToPool(jedis);
+        }
+    }
+
+    @Override
+    public boolean delete(KeyPrefix prefix, String key) {
+        Jedis jedis = null;
+        try {
+            jedis = jedisPool.getResource();
+            String realKey = prefix.getPrefix() + key;
+            long ret = jedis.del(realKey);
+            return ret > 0;
+        } finally {
+            returnToPool(jedis);
+        }
+    }
+
+
+    /**
+     * 回收redis连接
+     * @param jedis
+     */
+    private void returnToPool(Jedis jedis) {
+        if (jedis != null) {
+            jedis.close();
+        }
     }
 }
