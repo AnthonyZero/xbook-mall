02ac21617c7e69b30ecbaf068786b415ca44ae1e
pingjin <736252868@qq.com>
2019-08-14 15:27:29 +0800

    添加lombok

--- xbook-mall-service-redis/src/main/java/com/xbook/redis/config/RedisConfig.java
+++ xbook-mall-service-redis/src/main/java/com/xbook/redis/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.xbook.redis.config;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Component;
  */
 @Component
 @ConfigurationProperties(prefix = "redis")
+@Data
 public class RedisConfig {
     private String host;
     private int port;
@@ -16,60 +18,4 @@ public class RedisConfig {
     private int poolMaxTotal;
     private int poolMaxIdle;
     private int poolMaxWait;
-
-    public String getHost() {
-        return host;
-    }
-
-    public void setHost(String host) {
-        this.host = host;
-    }
-
-    public int getPort() {
-        return port;
-    }
-
-    public void setPort(int port) {
-        this.port = port;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public int getTimeout() {
-        return timeout;
-    }
-
-    public void setTimeout(int timeout) {
-        this.timeout = timeout;
-    }
-
-    public int getPoolMaxTotal() {
-        return poolMaxTotal;
-    }
-
-    public void setPoolMaxTotal(int poolMaxTotal) {
-        this.poolMaxTotal = poolMaxTotal;
-    }
-
-    public int getPoolMaxIdle() {
-        return poolMaxIdle;
-    }
-
-    public void setPoolMaxIdle(int poolMaxIdle) {
-        this.poolMaxIdle = poolMaxIdle;
-    }
-
-    public int getPoolMaxWait() {
-        return poolMaxWait;
-    }
-
-    public void setPoolMaxWait(int poolMaxWait) {
-        this.poolMaxWait = poolMaxWait;
-    }
 }
