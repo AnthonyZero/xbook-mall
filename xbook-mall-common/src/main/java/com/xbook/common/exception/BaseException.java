532aa5b01038bdd05f2dc3c0a569aa9c6a49ec8f
pingjin <736252868@qq.com>
2019-08-16 10:56:10 +0800

    基础异常 所有模块异常类基础它

--- xbook-mall-common/src/main/java/com/xbook/common/exception/BaseException.java
+++ xbook-mall-common/src/main/java/com/xbook/common/exception/BaseException.java
@@ -3,11 +3,15 @@ package com.xbook.common.exception;
 
 import com.xbook.common.enums.CodeMsgEnum;
 
-public class BusiException extends RuntimeException{
-    private static final long serialVersionUID = 1L;
+/**
+ * 模块业务异常都继承BaseException
+ */
+public class BaseException extends RuntimeException {
     private CodeMsgEnum codeMsgEnum;
 
-    public BusiException(CodeMsgEnum codeMsgEnum) {
+    public BaseException() { }
+
+    public BaseException(CodeMsgEnum codeMsgEnum) {
         super(codeMsgEnum.getMsg());
         this.codeMsgEnum = codeMsgEnum;
     }
