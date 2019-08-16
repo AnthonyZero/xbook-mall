593862e8fd3ac03070f7c4325d1758ff80bc50af
pingjin <736252868@qq.com>
2019-08-16 18:14:34 +0800

    代码生成工具

--- xbook-mall-dao/src/main/java/com/xbook/dao/CodeGenerator.java
+++ xbook-mall-dao/src/main/java/com/xbook/dao/CodeGenerator.java
@@ -24,13 +24,23 @@ public class CodeGenerator {
     private static String JDBC_URL = PropertiesFileUtil.getInstance("jdbc").get("jdbc.url");
     private static String JDBC_USERNAME = PropertiesFileUtil.getInstance("jdbc").get("jdbc.username");
     private static String JDBC_PASSWORD = PropertiesFileUtil.getInstance("jdbc").get("jdbc.password");
+    private static String[] projects = {"xbook-mall-entity", "xbook-mall-dao"}; //项目名
+    private static String[] parentNames = {"com.xbook.entity", "com.xbook.dao"}; //代码放置位置 包的父目录
 
     public static void main(String[] args) {
+        String moduleName = scanner("模块名");
+        String tableName = scanner("表名");
+        for(int i = 0; i < projects.length; i++) {
+            generator(projects[i], parentNames[i], moduleName, tableName);
+        }
+    }
+
+    private static void generator(String projectName, String parentName, String moduleName, String tableName) {
         // 代码生成器
         AutoGenerator mpg = new AutoGenerator();
         // 全局配置
         GlobalConfig gc = new GlobalConfig();
-        String projectPath = System.getProperty("user.dir") + "/xbook-mall-dao";
+        String projectPath = System.getProperty("user.dir") + "/" + projectName;
         gc.setOutputDir(projectPath + "/src/main/java");
         gc.setAuthor("anthonyzero");
         gc.setOpen(false); //是否打开输出目录
@@ -51,44 +61,58 @@ public class CodeGenerator {
 
         // 包配置
         PackageConfig pc = new PackageConfig();
-        pc.setModuleName(scanner("模块名")); //在哪个模块下
-        pc.setParent("com.xbook.dao");
+        /*pc.setModuleName(moduleName);*/ //在哪个模块下 比如pay
+        pc.setParent(parentName);
+        pc.setEntity(moduleName); // 默认为entity
+        pc.setMapper(moduleName); // 默认为mapper
         mpg.setPackageInfo(pc);
 
-        // 可注入自定义参数等操作以实现个性化操作
-        InjectionConfig cfg = new InjectionConfig() {
-            //自定义属性注入:abc
-            //在.ftl(或者是.vm)模板中，通过${cfg.abc}获取属性
-            @Override
-            public void initMap() {
-                // to do nothing
-            }
-        };
-
-        // 如果模板引擎是 freemarker
-        String templatePath = "/templates/mapper.xml.ftl"; //此文件是mybatis-plus-generator自带模板
-        // 自定义输出配置
-        List<FileOutConfig> focList = new ArrayList<>();
-        // 自定义配置会被优先输出
-        focList.add(new FileOutConfig(templatePath) {
-            @Override
-            public String outputFile(TableInfo tableInfo) {
-                // 自定义输出xml文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
-                return projectPath + "/src/main/resources/mapper/" + pc.getModuleName()
-                        + "/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
-            }
-        });
-        cfg.setFileOutConfigList(focList);
-        mpg.setCfg(cfg);
 
         //TemplateConfig 可自定义代码生成的模板
         // 配置模板
-        TemplateConfig templateConfig = new TemplateConfig();
-        templateConfig.setService(null);  //不生成service代码
-        templateConfig.setServiceImpl(null); //不生成serviceImpl代码
-        templateConfig.setController(null); //不生成controller代码
-        templateConfig.setXml(null);
-        mpg.setTemplate(templateConfig);
+        if ("xbook-mall-dao".equals(projectName)) {
+            // 可注入自定义参数等操作以实现个性化操作
+            InjectionConfig cfg = new InjectionConfig() {
+                //自定义属性注入:abc
+                //在.ftl(或者是.vm)模板中，通过${cfg.abc}获取属性
+                @Override
+                public void initMap() {
+                    // to do nothing
+                }
+            };
+
+            // 如果模板引擎是 freemarker
+            String templatePath = "/templates/mapper.xml.ftl"; //此文件是mybatis-plus-generator自带模板
+            // 自定义输出配置
+            List<FileOutConfig> focList = new ArrayList<>();
+            // 自定义配置会被优先输出
+            focList.add(new FileOutConfig(templatePath) {
+                @Override
+                public String outputFile(TableInfo tableInfo) {
+                    // 自定义输出xml文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
+                    return projectPath + "/src/main/resources/mapper/" + moduleName
+                            + "/" + tableInfo.getMapperName() + StringPool.DOT_XML;
+                }
+            });
+            cfg.setFileOutConfigList(focList);
+            mpg.setCfg(cfg);
+
+            TemplateConfig templateConfig = new TemplateConfig();
+            templateConfig.setEntity(null); //不生成entity代码
+            templateConfig.setService(null);  //不生成service代码
+            templateConfig.setServiceImpl(null); //不生成serviceImpl代码
+            templateConfig.setController(null); //不生成controller代码
+            templateConfig.setXml(null);
+            mpg.setTemplate(templateConfig);
+        } else {
+            TemplateConfig templateConfig = new TemplateConfig();
+            templateConfig.setMapper(null); //不生成mapper代码
+            templateConfig.setService(null);  //不生成service代码
+            templateConfig.setServiceImpl(null); //不生成serviceImpl代码
+            templateConfig.setController(null); //不生成controller代码
+            templateConfig.setXml(null);
+            mpg.setTemplate(templateConfig);
+        }
 
         // 策略配置
         StrategyConfig strategy = new StrategyConfig();
@@ -96,17 +120,16 @@ public class CodeGenerator {
         strategy.setColumnNaming(NamingStrategy.underline_to_camel);
         strategy.setEntityLombokModel(true);// lombok注解
         // 写于父类中的公共字段
-        strategy.setInclude(scanner("表名")); //需要包含的表名
+        strategy.setInclude(tableName); //需要包含的表名
         /*strategy.setSuperEntityColumns("id");*/ //主键为id的时候 生成的代码不会包含主键id 去掉
         strategy.setControllerMappingHyphenStyle(true);
-        /*strategy.setTablePrefix(pc.getModuleName() + "_");*/  //绝对生成的实体是否带前缀 如果设置了值 会去掉前缀（值）
+        /*strategy.setTablePrefix(pc.getModuleName() + "_");*/  //决定生成的实体类是否带前缀 如果设置了值 会去掉前缀（值） 这里不去掉前缀
         strategy.setEntityTableFieldAnnotationEnable(true); //是否生成实体时，生成字段注解
         mpg.setStrategy(strategy);
         // 选择 freemarker 引擎需要指定如下加，注意 pom 依赖必须有！
         mpg.setTemplateEngine(new FreemarkerTemplateEngine());
         mpg.execute();
     }
-
 
     public static String scanner(String tip) {
         Scanner scanner = new Scanner(System.in);
