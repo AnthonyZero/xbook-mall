dddb1bc9e8eaa60ef9b2b3cc6a39f56499ce6a01
pingjin <736252868@qq.com>
2019-08-16 15:56:00 +0800

    web service

---
+++ xbook-mall-web/src/main/java/com/xbook/web/StartedUpWebApplication.java
@@ -0,0 +1,12 @@
+package com.xbook.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartedUpWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartedUpWebApplication.class, args);
+    }
+}
