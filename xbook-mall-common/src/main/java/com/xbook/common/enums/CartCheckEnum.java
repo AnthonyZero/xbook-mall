62b36c7abc3669126e05b4f1321fb04a00f31bff
pingjin <736252868@qq.com>
2019-08-23 15:33:47 +0800

    commit

--- xbook-mall-common/src/main/java/com/xbook/common/enums/CartCheckEnum.java
+++ xbook-mall-common/src/main/java/com/xbook/common/enums/CartCheckEnum.java
@@ -3,7 +3,7 @@ package com.xbook.common.enums;
 import lombok.Getter;
 
 @Getter
-public enum CartCheck {
+public enum CartCheckEnum {
 
     CHECKED(1, "选中"),
     UN_CHECKED(0, "未选中"),
@@ -12,7 +12,7 @@ public enum CartCheck {
     private int code;
     private String msg;
 
-    CartCheck(int code, String msg) {
+    CartCheckEnum(int code, String msg) {
         this.code = code;
         this.msg = msg;
     }
